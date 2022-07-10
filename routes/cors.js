4810b7f47be15027c689b8116df6c2c05bfd26ae
nitesh sinwar <niteshsinwar@gmail.com>
2022-07-10 09:42:42 +0530

    commit

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['https://localhost:3000','http://localhost:3000', 'http://localhost:3443','https://localhost:3443', 'http://CEO-NITESH'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
